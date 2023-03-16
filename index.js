fd87f5f5291d093a00d0c4d94c358e673ac3f180
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-03-17 01:08:25 +0300

    functions attempt

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks(){
+    return (50-42)
+}
+
+function distanceFromHqInFeet(){
+    return ((264*8))
+}
+
+function distanceTravelledInFeet() {
+    return(264*5)
+}
+
+function calculatesFarePrice(){
+    return (2.56)
+}
